634f556115d9fedcc860490c3ea6432ab4ec80ef
grrkekisbest <97322615+grrkekisbest@users.noreply.github.com>
2023-07-12 04:44:33 -0400

    Update HowMuch.js

--- HowMuch.js
+++ HowMuch.js
@@ -4,6 +4,7 @@
 let hold = [];
 let Kib = 0;
 let Mib = 0;
+let GB = 0;
 let y = document.getElementById("list")
 for (let index = 1; index <= (y.children[1].childElementCount)-1; index++) {
     hold.push(y.children[1].children[index].children[1])
@@ -12,7 +13,11 @@ for (let index = 0; index <= hold.length-1; index++) {
     if((hold[index].textContent.split(" ")[1]) === "KiB"){
     	Kib += parseFloat(hold[index].textContent.split(" ")[0])
     }
+    else if((hold[index].textContent.split(" ")[1]) === "GiB"){
+    	GB += parseFloat(hold[index].textContent.split(" ")[0])
+    }
     else{
     	Mib += parseFloat(hold[index].textContent.split(" ")[0])
     }
 }
+console.log(((Mib/1000)+GB+(Kib/976563)).toFixed(2) + " Gb")
